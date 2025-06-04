214435fc4d2f52512a82ba3e42aa94d04f2f475e
Farzan <56518530+FarzanAli@users.noreply.github.com>
2025-06-04 15:19:05 -0400

    Time: 9 ms (52.12%) | Memory: 18.7 MB (34.92%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -2,10 +2,14 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         
+        # We can use a BFS approach where we check the
+        # biggest jump we can take "level" by "level"
         l, r = 0, 0
         count = 0
 
         while r < len(nums) - 1:
+            # farthest computes the greatest jump you can take
+            # in this step which is used as the new right pointer.
             farthest = nums[l] + l
             for i in range(l, r + 1):
                 farthest = max(farthest, i + nums[i])
