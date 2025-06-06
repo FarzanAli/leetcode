f06c8358cf554536e3619381be99b2753274892b
Farzan <56518530+FarzanAli@users.noreply.github.com>
2025-06-06 15:26:33 -0400

    Time: 0 ms (100.00%) | Memory: 19.8 MB (19.74%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -4,10 +4,13 @@ class Solution:
         newStart, newEnd = newInterval
         ans = []
 
+        # This loop helps us find the first overlapping interval
         while index < len(intervals) and newStart > intervals[index][1]:
             ans.append(intervals[index])
             index += 1
             
+        # This loop helps us find the rest of the overlapping intervals
+        # while updating what our new start and end times
         while index < len(intervals) and newEnd >= intervals[index][0]:
             newStart = min(newStart, intervals[index][0])
             newEnd = max(newEnd, intervals[index][1])
@@ -15,6 +18,7 @@ class Solution:
 
         ans.append([newStart, newEnd])
 
+        # Appends rest of the non overlapping intervals
         while index < len(intervals):
             ans.append(intervals[index])
             index += 1
