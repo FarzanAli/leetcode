86d1a331e7532e804ed8754c804b128d77e752b7
Farzan <56518530+FarzanAli@users.noreply.github.com>
2025-06-08 13:07:29 -0400

    Time: 319 ms (34.74%) | Memory: 59.5 MB (47.24%) - LeetSync

--- 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.py
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.py
@@ -9,12 +9,16 @@ class Solution:
 
         while queries:
             q = queries.pop()
+            # Create minHeap such that all intervals start before the current query value
+            # mantain each intervals size and end value
             while index < len(intervals) and intervals[index][0] <= q[0]:
                 heapq.heappush(minHeap, [intervals[index][1] - intervals[index][0] + 1, intervals[index][1]])
                 index += 1
-            
+            # Remove all the smallest intervals that have an end value less
+            # than the query value (irrelevant intervals)
             while minHeap and minHeap[0][1] < q[0]:
                 size, r = heapq.heappop(minHeap)
+            # If an interval exists that encompasses the query value, overwrite the -1
             if minHeap:
                 ans[q[1]] = minHeap[0][0]
         return ans
